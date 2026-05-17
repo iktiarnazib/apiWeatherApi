5a1ba0f66f468d9a61052d3ac8ef88e057a5f820
Iktiar Nazib <iktiarnazib@gmail.com>
2026-05-17 18:53:06 +0600

    Weather Model has been implemented

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,11 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String weatherCondition;
+
+  Weather({
+    required this.cityName,
+    required this.temperature,
+    required this.weatherCondition,
+  });
+}
