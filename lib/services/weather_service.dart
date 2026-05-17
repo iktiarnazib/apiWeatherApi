894385d7484ef35741bd6528bec51f95cfcdfcac
Iktiar Nazib <iktiarnazib@gmail.com>
2026-05-17 20:06:30 +0600

    Fetching city in weather service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -2,6 +2,8 @@
 import 'dart:convert';
 
 import 'package:apiweather/models/weather_model.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,14 +14,39 @@ class WeatherService {
   WeatherService(this.apiKey);
 
   Future<Weather> getWeather(String cityName) async {
-    final response = http.get(
+    final response = await http.get(
       Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=matric'),
     );
 
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception();
+      throw Exception('Failed to load weather data');
     }
+  }
+
+  //get current city
+  Future<String> getCurrentCity() async {
+    //get user permission for location
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    //fetch current location
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    //convert location into a list of placemark objects
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+
+    //extract the locality name from the first placemark
+    String? city = placemarks[0].locality;
+
+    return city ?? '';
   }
 }
